d1734be27a659395c0ec30e990c978d8795c842c
mash <maash@ua.edu>
2019-01-03 11:59:06 -0600

    material plug in

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import "./registerServiceWorker";
+import VueMDCAdapter from "vue-mdc-adapter";
+
+Vue.use(VueMDCAdapter);
 
 Vue.config.productionTip = false;
 
